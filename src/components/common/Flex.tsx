ee7d1e127aba6c46cf4b83f7a267eed163ce2501
nonon <nononcrust@gmail.com>
2023-12-16 23:49:39 +0900

    Feat: Flex 컴포넌트 추가

---
+++ src/components/common/Flex.tsx
@@ -0,0 +1,60 @@
+import { forwardRef } from 'react';
+import styled from 'styled-components';
+
+interface FlexProps extends React.ComponentPropsWithRef<'div'> {
+  justify?: 'center' | 'start' | 'end' | 'between' | 'around' | 'evenly';
+  items?: 'center' | 'start' | 'end';
+  flex?: 'row' | 'col';
+  wrap?: boolean;
+  gap?: number;
+  mt?: number;
+}
+
+const Flex = forwardRef<HTMLDivElement, FlexProps>(
+  ({ justify, items, flex = 'row', wrap = false, gap, mt, ...props }, ref) => {
+    return (
+      <FlexContainer
+        ref={ref}
+        justify={justify}
+        items={items}
+        flex={flex}
+        wrap={wrap}
+        gap={gap}
+        mt={mt}
+        {...props}
+      />
+    );
+  }
+);
+
+const styles = {
+  justify: {
+    center: 'center',
+    start: 'flex-start',
+    end: 'flex-end',
+    between: 'space-between',
+    around: 'space-around',
+    evenly: 'space-evenly',
+  },
+  items: {
+    center: 'center',
+    start: 'flex-start',
+    end: 'flex-end',
+  },
+  flex: {
+    row: 'row',
+    col: 'column',
+  },
+};
+
+const FlexContainer = styled.div<FlexProps>`
+  display: flex;
+  justify-content: ${({ justify }) => justify && styles.justify[justify]};
+  align-items: ${({ items }) => items && styles.items[items]};
+  flex-direction: ${({ flex }) => flex && styles.flex[flex]};
+  flex-wrap: ${({ wrap }) => wrap && 'wrap'};
+  gap: ${({ gap }) => gap && `${gap}px`};
+  margin-top: ${({ mt }) => mt && `${mt}px`};
+`;
+
+export default Flex;
