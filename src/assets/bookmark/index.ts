1bf85a1dfdd8fb70290b80775f5bf63b5b292c5e
nonon <nononcrust@gmail.com>
2023-12-16 23:51:00 +0900

    Chore: 아이콘, 이미지 추가

---
+++ src/assets/bookmark/index.ts
@@ -0,0 +1,3 @@
+import { ReactComponent as BookmarkPlaceholder } from './bookmark-placeholder.svg';
+
+export { BookmarkPlaceholder };
--- src/assets/common/index.ts
+++ src/assets/common/index.ts
@@ -1,3 +1,13 @@
 import { ReactComponent as CheckMarkIcon } from './check-mark.svg';
+import { ReactComponent as BookmarkSolidIcon } from './bookmark-solid.svg';
+import { ReactComponent as BookmarkOutlinedIcon } from './bookmark-outlined.svg';
+import { ReactComponent as EnterPageIcon } from './enter-page.svg';
+import { ReactComponent as TooltipMarkIcon } from './tooltip-mark.svg';
 
-export { CheckMarkIcon };
+export {
+  CheckMarkIcon,
+  BookmarkSolidIcon,
+  BookmarkOutlinedIcon,
+  EnterPageIcon,
+  TooltipMarkIcon,
+};
