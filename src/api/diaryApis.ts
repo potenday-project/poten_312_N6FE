bc7d10860ee1ace89717bae7a2ac82b0ecb02222
김민경 <sparrowscout.0000@gmail.com>
2023-12-16 21:29:52 +0900

    Fix : fix api url

--- src/api/diaryApis.ts
+++ src/api/diaryApis.ts
@@ -10,16 +10,29 @@ export interface IEditDiary extends IDiaryData {
   diaryId: number;
 }
 
+export interface MonthlyDiaryRespose {
+  id: number;
+  emotion: string[];
+  summary: string;
+  content: string;
+  writingDay: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export const diaryApis = {
+  getMonthlyDiary: async (month: string): Promise<MonthlyDiaryRespose[]> =>
+    await instance.get(`/api/diary/list?month=${month}`),
+
   getDiary: async (diaryId: number) =>
-    await instance.get(`/api/dairy/${diaryId}`),
+    await instance.get(`/api/diary/${diaryId}`),
 
   postDiary: async (diaryData: IDiaryData) =>
-    await instance.post(`/api/dairy`, diaryData),
+    await instance.post(`/api/diary`, diaryData),
 
   editDiary: async (diaryData: IEditDiary) =>
-    await instance.put(`/api/dairy`, diaryData),
+    await instance.put(`/api/diary`, diaryData),
 
   deleteDiary: async (diaryId: number) =>
-    await instance.delete(`/api/dairy/${diaryId}`),
+    await instance.delete(`/api/diary/${diaryId}`),
 };
