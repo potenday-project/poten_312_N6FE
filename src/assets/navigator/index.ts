084aaf8556da42135de85f267b5ac718fef0e2bc
김민경 <sparrowscout.0000@gmail.com>
2023-12-17 12:54:09 +0900

    Feat : add navigator

---
+++ src/assets/navigator/index.ts
@@ -0,0 +1,15 @@
+import { ReactComponent as AnalyticsTab } from './analyze.svg';
+import { ReactComponent as HomeTab } from './home.svg';
+import { ReactComponent as MyPageTab } from './mypage.svg';
+import { ReactComponent as BookmarkTab } from './bookmark.svg';
+import { ReactComponent as WriteBtn } from './write.svg';
+import { ReactComponent as NavigatorBackground } from './navigator.svg';
+
+export {
+  AnalyticsTab,
+  HomeTab,
+  MyPageTab,
+  BookmarkTab,
+  WriteBtn,
+  NavigatorBackground,
+};
